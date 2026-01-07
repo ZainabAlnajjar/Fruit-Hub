32a4c3bade8d8f045cb3fe53e54441bc36f480e7
nada <nadamansoursamir@gmail.com>
2026-01-07 02:01:43 +0200

    feat: add brandon grotesque font

--- lib/util/assets.dart
+++ lib/util/assets.dart
@@ -7,7 +7,5 @@ class Assets {
   static final fruitBasketPage2 = '${baseImage}fruit_basket_page2.png';
   static final fruitDrops = '${baseImage}fruit_drops.png';
   static final fruitBasketPageShadow = '${baseImage}fruit_basket_page_shadow.png';
-  static final fruitShadow = '${baseImage}fruit_shadow.png';
-
 
 }
