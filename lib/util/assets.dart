8310e5e6c514541ea77e6509ddd8ca32c30a0d0f
nada <nadamansoursamir@gmail.com>
2026-01-07 02:28:53 +0200

    feat: add fruit colors

--- lib/util/assets.dart
+++ lib/util/assets.dart
@@ -3,8 +3,8 @@ class Assets {
 
   static final baseImage = 'assets/images/';
 
-  static final fruitBasketPage1 = '${baseImage}fruit_basket_page1.png';
-  static final fruitBasketPage2 = '${baseImage}fruit_basket_page2.png';
+  static final fruitBasketPage1 = '${baseImage}fruit_basket_page2.png';
+  static final fruitBasketPage2 = '${baseImage}fruit_basket_page1.png';
   static final fruitDrops = '${baseImage}fruit_drops.png';
   static final fruitBasketPageShadow = '${baseImage}fruit_basket_page_shadow.png';
 
