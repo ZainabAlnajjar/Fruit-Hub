e583ca7c33d1f6bcc6f748a8954a35bdc909eacf
nada <nadamansoursamir@gmail.com>
2026-01-07 18:55:45 +0200

    refactor:on_boarding pages images to be responsive

--- lib/presentation/pages/on_boarding_page2.dart
+++ lib/presentation/pages/on_boarding_page2.dart
@@ -10,6 +10,7 @@ class OnBoardingPage2 extends StatelessWidget {
   Widget build(BuildContext context) {
     final media = MediaQuery.of(context);
     final height = media.size.height;
+    final width = media.size.width;
     return Scaffold(
       body: Column(
         children: [
@@ -27,7 +28,11 @@ class OnBoardingPage2 extends StatelessWidget {
                     child: Image.asset(Assets.fruitDrops),
                   ),
                 ),
-                Image.asset(Assets.fruitBasketPage2,fit: BoxFit.cover),
+                Image.asset(Assets.fruitBasketPage2,
+                    width: width * 0.5,
+                    height: height * 0.3,
+                    fit: BoxFit.contain
+                ),
                 SizedBox(height: 8),
                 Image.asset(Assets.fruitBasketPageShadow)
               ],
