a9cdae96edd4c037a84a4a8876bee6b2b0e7ca37
nada <nadamansoursamir@gmail.com>
2026-01-09 17:43:49 +0200

    feat: implement checkout and card payment bottom sheets

--- lib/presentation/components/card_payment_bottom_sheet.dart
+++ lib/presentation/components/card_payment_bottom_sheet.dart
@@ -9,92 +9,119 @@ class CardPaymentBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: GestureDetector(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Icon(Icons.close, color: Colors.black, size: 29),
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(Icons.close, color: Colors.black, size: 29),
+              ),
             ),
           ),
-        ),
-        SizedBox(height: 16),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(24),
-              topRight: Radius.circular(24),
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Delivery address",
-                  style: TextStyle(
-                    color: FruitColor.navyBlueColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
+          SizedBox(height: 16),
+          Flexible(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24),
+                  topRight: Radius.circular(24),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Card Holders Name",
+                        style: TextStyle(
+                          color: FruitColor.navyBlueColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      CustomTextField(hint: "Adolphus Chris"),
+                      SizedBox(height: 24),
+                      Text(
+                        "Card Number",
+                        style: TextStyle(
+                          color: FruitColor.navyBlueColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      CustomTextField(hint: "1234 5678 9012 1314"),
+                      SizedBox(height: 24),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Date",
+                                  style: TextStyle(
+                                    color: FruitColor.navyBlueColor,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(height: 16),
+                                CustomTextField(hint: "10/30"),
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 24),
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "CCV",
+                                  style: TextStyle(
+                                    color: FruitColor.navyBlueColor,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(height: 16),
+                                CustomTextField(hint: "123"),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 40),
+                      SizedBox(
+                        width: double.infinity,
+                        child: FruitPrimaryButton(
+                          buttonText: "Complete Order",
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 16),
-                CustomTextField(hint: "10th avenue, Lekki, Lagos State"),
-                SizedBox(height: 24),
-                Text(
-                  "Delivery address",
-                  style: TextStyle(
-                    color: FruitColor.navyBlueColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(height: 16),
-                CustomTextField(hint: "09090605708"),
-                SizedBox(height: 24),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Date",
-                      style: TextStyle(
-                        color: FruitColor.navyBlueColor,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    CustomTextField(hint: "10/30"),
-                    Text(
-                      "CCV",
-                      style: TextStyle(
-                        color: FruitColor.navyBlueColor,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    CustomTextField(hint: "123"),
-                  ],
-                ),
-                SizedBox(height: 40,),
-                FruitPrimaryButton(
-                  buttonText: "Complete Order",
-                  onPressed: () {},
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/presentation/components/checkout_bottom_sheet.dart
+++ lib/presentation/components/checkout_bottom_sheet.dart
@@ -10,84 +10,95 @@ class CheckOutBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: GestureDetector(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Icon(Icons.close, color: Colors.black, size: 29),
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(Icons.close, color: Colors.black, size: 29),
+              ),
             ),
           ),
-        ),
-        SizedBox(height: 16),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(24),
-              topRight: Radius.circular(24),
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Delivery address",
-                  style: TextStyle(
-                    color: FruitColor.navyBlueColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
+          SizedBox(height: 16),
+          Flexible(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24),
+                  topRight: Radius.circular(24),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Delivery address",
+                        style: TextStyle(
+                          color: FruitColor.navyBlueColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      CustomTextField(hint: "10th avenue, Lekki, Lagos State"),
+                      SizedBox(height: 24),
+                      Text(
+                        "Delivery address",
+                        style: TextStyle(
+                          color: FruitColor.navyBlueColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      CustomTextField(hint: "09090605708"),
+                      SizedBox(height: 40),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          FruitOutlinedButton(
+                            buttonText: "Pay on delivery",
+                            onPressed: () {},
+                          ),
+                          FruitOutlinedButton(
+                            buttonText: "Pay with card",
+                            onPressed: () {
+                              Navigator.pop(context);
+                              
+                              showModalBottomSheet(
+                                context: context,
+                                isScrollControlled: true,
+                                backgroundColor: Colors.transparent,
+                                builder: (context) {
+                                  return CardPaymentBottomSheet();
+                                },
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 16),
-                CustomTextField(hint: "10th avenue, Lekki, Lagos State"),
-                SizedBox(height: 24),
-                Text(
-                  "Delivery address",
-                  style: TextStyle(
-                    color: FruitColor.navyBlueColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(height: 16),
-                CustomTextField(hint: "09090605708"),
-                SizedBox(height: 40),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    FruitOutlinedButton(
-                      buttonText: "Pay on delivery",
-                      onPressed: () {},
-                    ),
-                    FruitOutlinedButton(
-                      buttonText: "Pay with card",
-                      onPressed: () {
-                        showModalBottomSheet(
-                          context: context,
-                          isScrollControlled: true,
-                          backgroundColor: Colors.transparent,
-                          builder: (context) {
-                            return CardPaymentBottomSheet();
-                          },
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
